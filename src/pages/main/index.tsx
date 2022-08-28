c1bd96407c778abad46d77b732fb90569aada45a
Dimka <dtkachev@project-llc.ru>
2022-08-28 12:53:59 +0300

    refactor(Main): add new structure

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,6 +1,26 @@
+import { palette } from '@my/ui-kit'
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+const Text = styled.p`
+  color: ${palette.RED};
+`
+
 export const Main = () => {
 
   return (
-    <p>asd</p>
+    <Wrapper>
+      <div>
+        <Text>qwe</Text>
+        <Text>zxc</Text>
+      </div>
+      
+      <div>
+        <Text>asd</Text>
+        <Text>eeee</Text>
+      </div>
+    </Wrapper>
   )
 }
